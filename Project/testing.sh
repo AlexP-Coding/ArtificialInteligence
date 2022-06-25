730711d332eb10f70c06626c9bcdf1bca609305d
Alexisminas <alexisminas308@gmail.com>
2022-06-26 00:44:21 +0100

    new testing script

---
+++ Project/testing.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# to run:
+# ./testing.sh testnr
+# ex:
+# ./testing.sh 01
+
+
+for inputfile in testes-takuzu/input_T$1
+do
+    myoutputfile=testes-takuzu/myout_T$1
+    outputfile=testes-takuzu/output_T$1
+    python3 takuzu3.py < $inputfile > $myoutputfile
+    diff $myoutputfile $outputfile
+done
+
