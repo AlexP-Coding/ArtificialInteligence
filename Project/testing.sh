3bae3f31bd6abdb8d24b99762634585b631699b9
Alexisminas <alexisminas308@gmail.com>
2022-06-26 02:12:22 +0100

    comparison file currently greedy search; testing file now prints metrics for both the original takuzu solver and the less efficient one

--- Project/testing.sh
+++ Project/testing.sh
@@ -9,7 +9,11 @@ for inputfile in testes-takuzu/input_T$1
 do
     myoutputfile=testes-takuzu/myout_T$1
     outputfile=testes-takuzu/output_T$1
-    python3 takuzu.py < $inputfile > $myoutputfile
+		echo 'DFS Takuzu:'
+    time python3 takuzu.py < $inputfile > $myoutputfile
+    diff $myoutputfile $outputfile
+		echo 'OTHER Takuzu:'
+		time python3 takuzu-other.py < $inputfile > $myoutputfile
     diff $myoutputfile $outputfile
 done
 
