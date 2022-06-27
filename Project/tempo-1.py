de11e01e37a3f084b6bcf427b4fe93e9d2df23c6
Inara Parbato <inaramussa@Air-de-Inara.Home>
2022-06-27 19:52:13 +0100

    ficheiro com os valores do tempo

---
+++ Project/tempo-1.py
@@ -0,0 +1,226 @@
+#*** DFS ***:
+
+#INPUT 1: 
+
+#User: 241
+
+#INPUT 2:
+
+#User:261
+
+#INPUT 3:
+
+#User:229
+
+#INPUT 4:
+
+#User:206
+
+#INPUT 5:
+
+#User:323
+
+#INPUT 6:
+
+#User:290
+
+#INPUT 7:
+
+#User:332
+
+#INPUT 8:
+
+#User:220
+
+
+#INPUT 9:
+
+#User:493
+
+
+#INPUT 10:
+
+#User:328
+
+#INPUT 11:
+
+#User:449
+
+#INPUT 12:
+
+#User:446
+
+#INPUT 13:
+
+#User:561
+
+#**** BFS *****:
+
+#INPUT 1: 
+
+#User: 313
+
+#INPUT 2:
+
+#User: 191
+
+#INPUT 3:
+
+#User: 308
+
+#INPUT 4:
+
+#User: 193
+
+#INPUT 5:
+
+#User: 228
+
+#INPUT 6:
+
+#User: 261
+
+#INPUT 7:
+
+#User: 193
+
+#INPUT 8:
+
+#User: 416
+
+
+#INPUT 9:
+
+#User: 373
+
+
+#INPUT 10:
+
+#User: 282
+
+#INPUT 11:
+
+#User: 543
+
+#INPUT 12:
+
+#User: 587
+
+#INPUT 13:
+
+#User: 460
+
+##**** GREEDY *****:
+
+#INPUT 1: 
+
+#user	0m0.236s
+
+#INPUT 2:
+
+#user	0m0.264s
+
+#INPUT 3:
+
+#user	0m0.231s
+
+#INPUT 4:
+
+#user	0m0.196s
+
+#INPUT 5:
+
+#user	0m0.255s
+
+#INPUT 6:
+
+#user	0m0.394s
+
+#INPUT 7:
+
+u#ser	0m0.342s
+
+#INPUT 8:
+
+#user	0m0.170s
+
+#INPUT 9:
+
+#user	0m0.408s
+
+#INPUT 10:
+
+#user	0m0.479s
+
+#INPUT 11:
+
+#user	0m0.409s
+
+#INPUT 12:
+
+#user	0m0.563s
+
+#INPUT 13:
+
+#user	0m0.255s
+
+#******  A*  :
+
+#INPUT 1: 
+
+#User: 315
+
+#INPUT 2:
+
+#User: 225
+
+#INPUT 3:
+
+#User: 384
+
+
+#INPUT 4:
+
+#User: 258
+
+#INPUT 5:
+
+#User: 232
+
+#INPUT 6:
+
+#User: 312
+
+#INPUT 7:
+
+#User: 375
+
+#INPUT 8:
+
+#User: 197
+
+#INPUT 9:
+
+#User: 341
+
+#INPUT 10:
+
+#User: 467
+
+#INPUT 11:
+
+#User: 522
+
+#INPUT 12:
+
+#User: 672
+
+#INPUT 13:
+
+#User: 367
+
+
+
+
+
+
